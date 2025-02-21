f17f7d0d9f614acafe7e099d1e22d1c830c612f7
zhangjiabo <812941112@qq.com>
2025-02-21 13:29:51 +0800

    try

--- build/dnf_data/home/template/init/run/start_monitor.sh
+++ build/dnf_data/home/template/init/run/start_monitor.sh
@@ -2,6 +2,6 @@
 
 killall -9 df_monitor_r
 rm -rf pid/*.pid
-LD_PRELOAD=/home/template/init/libhook.so ./df_monitor_r server start
+LD_PRELOAD=/home/template/init/libfd_monitor.so:/home/template/init/libhook.so ./df_monitor_r server start
 sleep 2
 cat pid/*.pid |xargs -n1 -I{} tail --pid={} -f /dev/null
